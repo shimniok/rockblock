4a2d3c83b332e49fe447932b52921acec7785e74
shimniok@gmail.com <shimniok@gmail.com>
2017-03-23 11:59:11 -0600

    revised to print only latest status unless history parameter specifies the number of most recent entries to display

--- status.py
+++ status.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
 import cgi
-import cgitb; cgitb.enable()  # for troubleshooting
+#import cgitb; cgitb.enable()  # for troubleshooting
 import json
 import csv
 
 result = [];
 
+form = cgi.FieldStorage()
+history = form.getvalue("history")
+
 # TODO: revise to use logfile and extended format
 
 with open('./status.d', 'rb') as f:
   entries = list(csv.reader(f))
-  for e in entries:
-    result.insert(0, {
+  
+  if history == None:
+    # current entry only
+    e = entries[len(entries)-1];
+    result.append({
       'time': e[0],
       'lat': e[1],
       'lng': e[2],
@@ -20,6 +26,24 @@ with open('./status.d', 'rb') as f:
       'course': e[4],
       'msg': e[5]
     })
+  elif history > 0:
+    # print number of entries specified by history param
+    maximum = int(history)
+    i = 0
+    for e in reversed(entries):
+      result.append({
+        'time': e[0],
+        'lat': e[1],
+        'lng': e[2],
+        'speed': e[3],
+        'course': e[4],
+        'msg': e[5]
+      })
+      i += 1
+      if i >= maximum:
+        break
+    # insert actual number of entries
+    result.insert(0, i)
   
 print "Content-type: application/json"
 print
