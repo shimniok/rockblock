2d058cfae802965951d03eecb5e0eab2cb9fd478
shimniok@gmail.com <shimniok@gmail.com>
2017-03-24 10:32:59 -0600

    modified to include course, speed; smush and truncate lat/lon

--- test2.py
+++ test2.py
@@ -5,25 +5,32 @@ import time
 url = 'http://nogales/rock/receive.py'
 
 points = [ 
-	{ 'lon': -104.932838, 'lat': 39.597551, 'speed': 5, 'course': 12 },
-	{ 'lon': -104.932823, 'lat': 39.598514, 'speed': 5, 'course': 2 },
-  { 'lon': -104.932373, 'lat': 39.599715, 'speed': 5, 'course': 2 },
-  { 'lon': -104.932359, 'lat': 39.601095, 'speed': 5, 'course': 46 },
-  { 'lon': -104.930727, 'lat': 39.600901, 'speed': 5, 'course': 73 }
+	{ 'lon': "-104.932838", 'lat': "39.597551", 'speed': 5, 'course': 12 },
+	{ 'lon': "-104.932823", 'lat': "39.598514", 'speed': 5, 'course': 2 },
+  { 'lon': "-104.932373", 'lat': "39.599715", 'speed': 5, 'course': 2 },
+  { 'lon': "-104.932359", 'lat': "39.601095", 'speed': 5, 'course': 46 },
+  { 'lon': "-104.930727", 'lat': "39.600901", 'speed': 5, 'course': 73 }
 ]
 
 momsn = 0
 
 for p in points:
 	time.sleep(3)
-	print p
-	mydata = '%s,%s,%s,%s' % (p['lat'], p['lon'], p['speed'], p['course'])
+	# split into integer/fractional parts
+	lat = p['lat'].split('.')
+	lon = p['lon'].split('.')
+	# N decimal digits
+	lat[1] = lat[1][:5]
+	lat[1] = lat[1][:5]
+	# put data back together, don't include '.' for lat/lon
+	mydata = '%s%s,%s%s,%s,%s' % (lat[0], lat[1], lon[0], lon[1], p['speed'], p['course'])
+	print mydata
 	message = {
 		'imei': '300234010753370',
 		'momsn': '12345',
 		'transmit_time': '12-10-10 10:41:50',
-		'iridium_latitude': '52.3867',
-		'iridium_longitude': '0.2938',
+		'iridium_latitude': '%s.%s'%(lat[0], lat[1][:4]),
+		'iridium_longitude': '%s.%s'%(lon[0], lon[1][:4]),
 		'iridium_cep': '8',
 		'data': mydata.encode('hex')
 	}
@@ -33,4 +40,4 @@ for p in points:
 #r = requests.post(url, data=message)
 
 # Response, status etc
-print r.text, r.status_code
+#print r.text, r.status_code
