c44083e06cd3b971e57e3df75e8975ae22227ab5
shimniok@gmail.com <shimniok@gmail.com>
2017-03-24 10:58:29 -0600

    created parsing function; added comments; disabled log writing temporarily

--- receive.py
+++ receive.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python
 
+## receive.py
+##
+## Expects post or get with parameters:
+##
+## imei : string
+## momsn : string
+## transmit_time : string
+## iridium_latitude : string
+## iridium_longitude : string
+## iridium_cep : string
+## data : hex-encoded string, format:
+##      lat: string as [-]NNNNNNN (not padded; 5 decimal digits only, no '.')
+##      lon: string as [-]NNNNNNNN (not padded; 5 decimal digits only, no '.')
+##      speed: string as integer, not padded
+##      course: string as integer, not padded
+
 import datetime
 import cgi
 import cgitb; cgitb.enable()  # for troubleshooting
+
+########################################################################
+# parseGeo: adds decimal to unpadded 5 decimal digit number string
+#
+def parseGeo(g):
+    r = list(g)
+    r.reverse()
+    r.insert(5, '.')
+    r.reverse()
+    s = ''.join(r)
+    return s
+
+########################################################################
+# Main code
 
 form = cgi.FieldStorage()
 
@@ -17,15 +47,16 @@ text = ''
 
 # Decode the data
 if (data != None):
-    text = data.decode('hex')
-    # TODO: parse out telemetry parameters & message (if any)
+    d = data.decode('hex').split(',')
+    lat = parseGeo(d[0])
+    lon = parseGeo(d[1])
+    speed = d[2]
+    course = d[3]
 
-# Write to log
-with open('rock.log', 'a') as mylog:
-    mylog.write('%s,%s,%s,%s,%s,%s,%s,%s\n' %
-        (datetime.datetime.now(), momsn, imei, transmit_time, iridium_latitude, iridium_longitude, iridium_cep, text ))
-
-# Determine what to do with it
+# Write status data
+#with open('rock.log', 'a') as mylog:
+#    mylog.write('%s,%s,%s,%s,%s,%s,%s,%s\n' %
+#        (datetime.datetime.now(), momsn, imei, transmit_time, iridium_latitude, iridium_longitude, iridium_cep, text ))
 
 print "Content-type: text/html"
 print
@@ -37,3 +68,4 @@ print """
 </body>
 </html>
 """
+
