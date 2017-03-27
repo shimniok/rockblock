8514293fa0f7eda8d4daeac495ac5f998ca61f8c
shimniok <shimniok@gmail.com>
2017-03-27 17:41:14 +0000

    threaded gpsd polling, rejecting nan speed results

--- mobile_client.py
+++ mobile_client.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python
 
-from gps3 import gps3
-gps_socket = gps3.GPSDSocket()
-ds = gps3.DataStream()
-gps_socket.connect()
-gps_socket.watch()
-for new_data in gps_socket:
-    if new_data:
-        ds.unpack(new_data)
-        print('Alt = ', ds.TPV['alt'])
-        print('Lat = ', ds.TPV['lat'])
-        print('Lon = ', ds.TPV['lon'])
-        print('Speed = ', ds.TPV['speed'])
-        print('Course = ', ds.TPV['track'])
-        
+from gps import *
+import time
+import threading
+import math
+
+gpsd = None
+
+class GpsPoller(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		global gpsd
+		gpsd = gps(mode=WATCH_ENABLE)
+		self.current_value = None
+		self.running = True
+
+	def run(self):
+		global gpsd
+		while gpsp.running:
+			gpsd.next()
+
+if __name__ == '__main__':
+	gpsp = GpsPoller()
+	try:
+		gpsp.start()
+		while True:
+			if not math.isnan(float(gpsd.fix.speed)):
+				print "Lat = ", gpsd.fix.latitude
+				print "Lon = ", gpsd.fix.longitude
+				print "Alt = ", gpsd.fix.altitude
+				print "Speed = ", gpsd.fix.speed
+				print "Track = ", gpsd.fix.track
+				print
+			time.sleep(5)
+	except (KeyboardInterrupt, SystemExit):
+		print "\nKilling thread..."
+		gpsp.running = False
+		gpsp.join()
+	print "Done.\nExiting."
